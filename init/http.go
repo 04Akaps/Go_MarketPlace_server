08640ad3edd8872b44757b3cf6977f4d5c900c2b
유호진 <hojin.yu@supercat.co.kr>
2023-03-20 11:31:22 +0900

    (feat) cors 기본적으로 설정

--- init/http.go
+++ init/http.go
@@ -2,6 +2,7 @@ package init
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"goServer/server"
 	"goServer/utils"
 	"log"
@@ -16,16 +17,19 @@ func HttpServerInit(port string) error {
 
 func registerHttpRouter() http.Handler {
 	router := mux.NewRouter()
+	c := cors.AllowAll() // 일단 개발 편의상을 위해 전체 수용
 
 	registerSubRouterTest(router)
 
 	logMux := utils.LoggingMiddleware(router)
 	// 라우팅 관련해서는 Mux쓰는 것이 훨씬 깔끔하고 좋다고 생각하기 떄문에 Mux로 관리
 
-	return logMux
+	corsRouter := c.Handler(logMux)
+	return corsRouter
 }
 
 func registerSubRouterTest(router *mux.Router) {
+	// 나중에 health Check용으로 사용 할 수도??
 	testRouter := router.PathPrefix("/test").Subrouter()
 	testRouter.HandleFunc("", server.NewGetTest().ServeHTTP).Methods("GET")
 }
