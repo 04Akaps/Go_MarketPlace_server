84480d9b7e149bd300c22bc4897f909baf1d6f2b
유호진 <hojin.yu@supercat.co.kr>
2023-03-24 21:52:46 +0900

    (feat) VerifyToken추가

--- init/pasetoToken.go
+++ init/pasetoToken.go
@@ -13,6 +13,7 @@ type PasetoMaker struct {
 }
 
 type payload struct {
+	userName     string
 	currentTime  time.Time
 	randomString string
 }
@@ -24,7 +25,7 @@ func NewPasetoMaker(key string) *PasetoMaker {
 	}
 }
 
-func (maker *PasetoMaker) CreateToken() (string, error) {
+func (maker *PasetoMaker) CreateToken(userName string) (string, error) {
 	randomBytes := make([]byte, 16)
 	rand.Read(randomBytes)
 
@@ -33,7 +34,24 @@ func (maker *PasetoMaker) CreateToken() (string, error) {
 	newPayload := &payload{
 		currentTime:  time.Now(),
 		randomString: randomString,
+		userName:     userName,
 	}
 
 	return maker.Paseto.Encrypt(maker.Key, newPayload, nil)
+}
+
+func (maker *PasetoMaker) VerifyToken(token, userName string) (bool, error) {
+	payload := &payload{}
+
+	err := maker.Paseto.Decrypt(token, maker.Key, payload, nil)
+
+	if err != nil {
+		return false, err
+	}
+
+	if payload.userName == userName {
+		return true, nil
+	}
+
+	return false, err
 }
